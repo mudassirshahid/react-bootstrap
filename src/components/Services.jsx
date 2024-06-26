624324aea88f20c72ea91110bb6e49bc53b071e5
Mudassir <mudassirshahid84@gmail.com>
2024-06-26 15:33:17 +0500

    add comment

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Home from './Home'
 
 const Services = () => {
   return (
-    // <div>
-    //   services
-    // </div>
+    <div>
+      <Home />
+      {/* <h1>services</h1> */}
+    </div>
   )
 }
 
